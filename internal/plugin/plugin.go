8224a8a8635c1a248e3fff16b96ae0e627059aee
Yevhen Surovskyi <surovskyi.y@ajax.systems>
2025-03-17 18:35:46 +0200

    up

--- internal/plugin/plugin.go
+++ internal/plugin/plugin.go
@@ -8,6 +8,7 @@ import (
 	"github.com/argoproj/argo-rollouts/utils/rollout"
 	"github.com/sirupsen/logrus"
 	"net/http"
+	"reflect"
 )
 
 var _ rpc.TrafficRouterPlugin = &RpcPlugin{}
@@ -23,35 +24,74 @@ func (p *RpcPlugin) InitPlugin() pluginTypes.RpcError {
 
 func (r *RpcPlugin) SetWeight(ro *v1alpha1.Rollout, desiredWeight int32, additionalDestinations []v1alpha1.WeightDestination) pluginTypes.RpcError {
 	r.LogCtx.WithFields(logrus.Fields{
-		"rollout":                ro,
+		"rollout status":         ro.Status,
 		"desiredWeight":          desiredWeight,
 		"additionalDestinations": additionalDestinations,
 	}).Info("SetWeight called")
 
-	if rollout.IsFullyPromoted(ro) {
-		r.LogCtx.Info("Rollout is fully promoted, skipping weight update")
+	// This checks that we are performing a canary rollout, it is not
+	// an error if this is empty. This will be empty on the initial rollout
+	if reflect.DeepEqual(ro.Status.Canary, v1alpha1.CanaryStatus{}) {
+		r.LogCtx.WithFields(logrus.Fields{"desiredWeight": desiredWeight}).Debug("Rollout does not have a CanaryStatus yet")
 		return pluginTypes.RpcError{}
 	}
 
-	if !rollout.IsUnpausing(ro) {
+	if rolloutAborted(ro) {
+		r.LogCtx.Info("Rollout is aborted, skipping weight update")
+		//todo: revert weights
+		return pluginTypes.RpcError{}
+	}
+
+	if rollout.IsFullyPromoted(ro) {
+		r.LogCtx.Info("Rollout is fully promoted, skipping weight update")
+		if ro.Status.Canary.Weights.Canary.Weight == 0 {
+			r.LogCtx.Info("Rollout is fully promoted and weight is 0, skipping weight update")
+			if ro.Status.Canary.Weights.Stable.Weight == 100 {
+				rpcError, done := r.updateWeightsOnNats(ro.Name+"-"+ro.Status.CurrentPodHash, 1)
+				if done {
+					return rpcError
+				}
+			}
+		}
+		return pluginTypes.RpcError{}
+	}
+
+	if ro.Status.ControllerPause {
 		r.LogCtx.Info("Rollout is pausing, skipping weight update")
 		return pluginTypes.RpcError{}
 	}
 
-	url := fmt.Sprintf("http://host.minikube.internal:8222/debug/weight?weight=%d&desc=%s", desiredWeight, ro.Name+"-"+ro.Status.CurrentPodHash)
+	r.LogCtx.WithFields(logrus.Fields{
+		"rollout": ro,
+	}).Info("SetWeight called deep")
+
+	rpcError, doneCanary := r.updateWeightsOnNats(ro.Name+"-"+ro.Status.CurrentPodHash, desiredWeight)
+	if doneCanary {
+		return rpcError
+	}
+
+	rpcError, doneStable := r.updateWeightsOnNats(ro.Name+"-"+ro.Status.StableRS, 100-desiredWeight)
+	if doneStable {
+		return rpcError
+	}
+
+	return pluginTypes.RpcError{}
+}
+
+func (r *RpcPlugin) updateWeightsOnNats(rsName string, desiredWeight int32) (pluginTypes.RpcError, bool) {
+	url := fmt.Sprintf("http://host.minikube.internal:8222/debug/weight?weight=%d&desc=%s", desiredWeight, rsName)
 	r.LogCtx.WithField("url", url).Info("sending weight request")
 
 	resp, err := http.Get(url)
 	if err != nil {
-		return pluginTypes.RpcError{ErrorString: fmt.Sprintf("failed to send weight request: %v", err)}
+		return pluginTypes.RpcError{ErrorString: fmt.Sprintf("failed to send weight request: %v", err)}, true
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return pluginTypes.RpcError{ErrorString: fmt.Sprintf("weight request failed with status: %d", resp.StatusCode)}
+		return pluginTypes.RpcError{ErrorString: fmt.Sprintf("weight request failed with status: %d", resp.StatusCode)}, true
 	}
-
-	return pluginTypes.RpcError{}
+	return pluginTypes.RpcError{}, false
 }
 
 func (r *RpcPlugin) SetHeaderRoute(ro *v1alpha1.Rollout, headerRouting *v1alpha1.SetHeaderRoute) pluginTypes.RpcError {
@@ -86,19 +126,6 @@ func (r *RpcPlugin) Type() string {
 	return "plugin-nats"
 }
 
-func getCanaryIngressName(rollout *v1alpha1.Rollout, stableIngress string) string {
-	// names limited to 253 characters
-	if rollout.Spec.Strategy.Canary != nil &&
-		rollout.Spec.Strategy.Canary.TrafficRouting != nil &&
-		rollout.Spec.Strategy.Canary.TrafficRouting.Plugins != nil &&
-		rollout.Spec.Strategy.Canary.TrafficRouting.Plugins["argoproj/sample-nats"] != nil {
-
-		prefix := fmt.Sprintf("%s-%s", rollout.GetName(), stableIngress)
-		if len(prefix) > 253-len("-canary") {
-			// trim prefix
-			prefix = prefix[0 : 253-len("-canary")]
-		}
-		return fmt.Sprintf("%s%s", prefix, "-canary")
-	}
-	return ""
+func rolloutAborted(rollout *v1alpha1.Rollout) bool {
+	return rollout.Status.Abort
 }
